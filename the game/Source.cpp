00c22bddb176443a5932c9b21e378aa77483b337
Amr Saeed <Amrsaeed@aucegypt.edu>
2016-05-05 16:47:33 +0200

    Ndafa

--- the game/Source.cpp
+++ the game/Source.cpp
@@ -2,39 +2,76 @@
 #include<vector>
 #include<cmath>
 #include<set>
+
 using namespace std;
+
+int WinWidth = 1280;
+int WinHeight = 720;
+
 struct point {
+
 private:
-	double x, y, z;
+	double x;
+	double y;
+	double z;
 public:
-	point(double s[3]) :x(s[0]), y(s[1]), z(s[2])
-	{}
-	point(double xx=0, double yy=0, double zz=0) :x(xx), y(yy), z(zz)
-	{}
-	point(point s, point s1) :x(s1[0] - s[0]), y(s1[1] - s[1]), z(s1[2] - s[2])
-	{}
+	point(double Points[3]):x(Points[0]), y(Points[1]), z(Points[2]){} //Construct From Array
+	point(double PointX = 0, double PointY = 0, double PointZ = 0) :x(PointX), y(PointY), z(PointZ){} //Constructor with 3 arguments
+	point(point PointA, point PointB) :x(PointB[0] - PointA[0]), y(PointB[1] - PointA[1]), z(PointB[2] - PointA[2]){} //Merge two points into one
+
 	double operator[](int index)
 	{
 		if (index == 0)
 			return x;
+
 		if (index == 1)
 			return y;
+
 		if (index == 2)
 			return z;
 	}
+
+	point& point::operator=(const point& a)
+	{
+		x = a.x;
+		y = a.y;
+		z = a.z;
+
+		return *this;
+	}
+
+	bool operator==(const point& a)
+	{
+		if (x != a.x)
+			return false;
+
+		if (y != a.y)
+			return false;
+
+		if (z != a.z)
+			return false;
+
+		return true;
+	}
+
+	bool operator!=(const point&a)
+	{
+		return !(*this == a);
+	}
+
+	double* point::data()
+	{
+		double a[] = { x,y,z };
+		return a;
+	}
+
 	/*void print() {
-		cout << x << " " << y << " " << z << endl;
+	cout << x << " " << y << " " << z << endl;
 	}*/
-	bool operator == (point& f)
-	{
-		return x == f.x&&y == f.y&&z == f.z;
-	}
-	bool operator !=(point&f)
-	{
-		return !(*this == f);
-	}
 };
+
 static set<sprite*> ListOfMovingObjects;
+
 class sprite{
 public:
 	sprite(vector<point>& m,vector<vector<int>>& s):points(move(m)),faces(move(s)) {
@@ -46,45 +83,46 @@ public:
 		ListOfMovingObjects.erase(this);
 	}
 private:
-	point place;
-	point box[4];
-	vector<point> points;
+	point Center;
+	point Box[4];
+	vector<point> Vertices;
 	vector<vector<int>> faces;
 };
+
 static set<sprite*> ListOfMovingObjects;
-class human :private sprite {
+
+class Human :private sprite {
 public:
-	human(vector<point>& m,vector<vector<int>>& s):sprite(m,s) {
-
-	}
-
+	Human(vector<point>& Vertices, vector<vector<int>>& Faces):sprite(Vertices,Faces) {}
 };
-class player:private human {
 
+class player:private Human {
 public:
-	player(vector<point>& s,vector<vector<int>>& f):human(s,f){
-		
-	}
+	player(vector<point>& Vertices, vector<vector<int>>& Faces):Human(Vertices, Faces){}
 };
+
 class enemy :private sprite {
 	 
 };
-class bullet : protected sprite {
+
+class bullet :private sprite {
 
 };
+
 class app {
 	
 };
+
 class world {
 
 };
+
 int main(int argc, char *argv[])
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGB|GLUT_DOUBLE);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
 	glutInitWindowPosition(0,0);
-	glutInitWindowSize(1280, 720);
+	glutInitWindowSize(WinWidth, WinHeight);
 	glutCreateWindow("The Game");
 	
-
 }
